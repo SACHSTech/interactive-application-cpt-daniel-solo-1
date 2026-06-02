3e1b0d1d4086d5abda7d5e1595903957393679b6
Danook09 <daniel.marino27@ycdsbk12.ca>
2026-06-02 12:51:49 -0400

    fix chest and construct fish

--- src/Sketch.java
+++ src/Sketch.java
@@ -16,21 +16,63 @@ public class Sketch extends PApplet {
 
     @Override
     public void setup() {
+        
+        tankBase();
+        trasureChestBase();
+    }
+
+    private void trasureChestBase(){
+        fill(93, 64, 55);           // Treasure Chest
+        stroke(0,0,0);
+        rect(400, 250, 100, 50);
+        fill(255, 235, 59);
+        stroke(0,0,0);
+        rect(390, 240, 120, 10);
+        fill(255, 235, 59);
+        stroke(0,0,0);
+        rect(440, 240, 20, 20);
+    }
+
+    private void tankBase(){
         fill(154, 205, 248);        // Water Background
         noStroke();
         rect(0, 0, 600, 400);
 
-        fill(114, 97, 55); // Gravel Ground
+        fill(114, 97, 55);          // Gravel Ground
         stroke(0, 0, 0);
         rect(0, 250, 600, 200);
 
-        fill(0,0,0);
+        fill(0,0,0);                // Tank Lid
         noStroke();
-        rect(0, 0, 100, 15);
+        rect(0, 0, 600, 10);
     }
 
     @Override
     public void draw() {
+
+        fill(93, 64, 55);           // Treasure Chest Lid
+        stroke(0,0,0);
+        rect(400, 219, 100, 20);
+
+        if (keyPressed){
+            if (keyCode == UP){
+                fill(93, 64, 55);           // Treasure Chest Lid
+                stroke(0,0,0);
+                rect(400, 179, 100, 60);
+                
+                fill(141, 110, 99);
+                stroke(0,0,0);
+                rect(405, 184, 90, 50);
+            }
+        }
+
+        //if (keyReleased){
+            //if (keyCode == UP){
+                //fill(93, 64, 55);
+                //stroke(0,0,0);
+                //rect(400, 209, 100, 30);
+            //}
+        //}
 
     }
 
