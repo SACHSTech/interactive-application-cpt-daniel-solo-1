b43059e110e01ba4f9bbdc5a823c5b24878f68c7
Danook09 <daniel.marino27@ycdsbk12.ca>
2026-06-03 14:19:13 -0400

    Begin adding fish and work on fish movement

--- src/Sketch.java
+++ src/Sketch.java
@@ -19,18 +19,35 @@ public class Sketch extends PApplet {
         
         tankBase();
         trasureChestBase();
+
     }
 
-    private void trasureChestBase(){
-        fill(93, 64, 55);           // Treasure Chest
-        stroke(0,0,0);
-        rect(400, 250, 100, 50);
-        fill(255, 235, 59);
-        stroke(0,0,0);
-        rect(390, 240, 120, 10);
-        fill(255, 235, 59);
-        stroke(0,0,0);
-        rect(440, 240, 20, 20);
+    @Override
+    public void draw() {
+
+        if (keyPressed){
+            if (keyCode == UP){
+                fill(93, 64, 55);           // Treasure Chest Lid
+                stroke(0,0,0);
+                rect(400, 179, 100, 60);
+                
+                fill(141, 110, 99);
+                stroke(0,0,0);
+                rect(405, 184, 90, 50);
+
+            }
+
+            if (keyCode == DOWN){
+                fill(154, 205, 248);
+                noStroke();
+                rect(390, 169, 120, 60);
+                
+                fill(93, 64, 55);           // Treasure Chest Lid
+                stroke(0,0,0);
+                rect(400, 219, 100, 20);
+            }
+        }
+
     }
 
     private void tankBase(){
@@ -46,34 +63,20 @@ public class Sketch extends PApplet {
         noStroke();
         rect(0, 0, 600, 10);
     }
-
-    @Override
-    public void draw() {
-
+    
+    private void trasureChestBase(){
+        fill(93, 64, 55);           // Treasure Chest
+        stroke(0,0,0);
+        rect(400, 250, 100, 50);
+        fill(255, 235, 59);
+        stroke(0,0,0);
+        rect(390, 240, 120, 10);
+        fill(255, 235, 59);
+        stroke(0,0,0);
+        rect(440, 230, 20, 20);
         fill(93, 64, 55);           // Treasure Chest Lid
         stroke(0,0,0);
         rect(400, 219, 100, 20);
-
-        if (keyPressed){
-            if (keyCode == UP){
-                fill(93, 64, 55);           // Treasure Chest Lid
-                stroke(0,0,0);
-                rect(400, 179, 100, 60);
-                
-                fill(141, 110, 99);
-                stroke(0,0,0);
-                rect(405, 184, 90, 50);
-            }
-        }
-
-        //if (keyReleased){
-            //if (keyCode == UP){
-                //fill(93, 64, 55);
-                //stroke(0,0,0);
-                //rect(400, 209, 100, 30);
-            //}
-        //}
-
     }
 
     /** Additional helper methods below */
